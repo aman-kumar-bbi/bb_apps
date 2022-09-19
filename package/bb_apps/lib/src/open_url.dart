224107629bc6995ccd3421b2d820a9245ed22379
aman-kumar-bbi <aman.kumar@media4u.com>
2022-09-19 10:00:12 +0530

    url test

--- package/bb_apps/lib/src/open_url.dart
+++ package/bb_apps/lib/src/open_url.dart
@@ -8,24 +8,33 @@ void openPlayStore(String urla, BuildContext context) async {
       androidApplicationId: urla,
     );
   } else {
-    final url =
-        Uri.parse("https://play.google.com/store/apps/details?id=$urla");
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      snackBar(context);
+    // final url =
+    //     Uri.parse("https://play.google.com/store/apps/details?id=$urla");
+    // if (await canLaunchUrl(url)) {
+    //   await launchUrl(url);
+    // } else {
+    //   snackBar(context);
+    // }
+    final url = "https://play.google.com/store/apps/details?id=$urla";
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
     }
   }
 }
 
 void openAppStore(String id, BuildContext context) async {
-  final url =
-      Uri.parse("https://apps.apple.com/in/story/id$id?itscg=10000&itsct=");
-  if (await canLaunchUrl(url)) {
-    await launchUrl(url);
-  } else {
-    snackBar(context);
-  }
+  // final url =
+  //     Uri.parse("https://apps.apple.com/in/story/id$id?itscg=10000&itsct=");
+  // if (await canLaunchUrl(url)) {
+  //   await launchUrl(url);
+  // } else {
+  //   snackBar(context);
+  // }
 }
 
 snackBar(BuildContext context) {
