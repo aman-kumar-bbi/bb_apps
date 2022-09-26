16246a6c67b195025933f95b9db8ba7108067f87
aman-kumar-bbi <aman.kumar@media4u.com>
2022-09-26 12:31:37 +0530

    some changes in url

--- package/bb_apps/lib/src/open_url.dart
+++ package/bb_apps/lib/src/open_url.dart
@@ -8,22 +8,15 @@ void openPlayStore(String urla, BuildContext context) async {
       androidApplicationId: urla,
     );
   } else {
-    // final url =
-    //     Uri.parse("https://play.google.com/store/apps/details?id=$urla");
-    // if (await canLaunchUrl(url)) {
-    //   await launchUrl(url);
-    // } else {
-    //   snackBar(context);
-    // }
     final url = "https://play.google.com/store/apps/details?id=$urla";
     if (await url_luncher.canLaunch(url)) {
       await url_luncher.launch(
         url,
 
         // mode: url_luncher.LaunchMode.externalApplication
-        // forceSafariVC: false,
-        // forceWebView: false,
-        // headers: <String, String>{'my_header_key': 'my_header_value'},
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
       );
     } else {
       snackBar(context);
@@ -32,12 +25,15 @@ void openPlayStore(String urla, BuildContext context) async {
 }
 
 void openAppStore(String id, BuildContext context) async {
-  // final url =
-  //     Uri.parse("https://apps.apple.com/in/story/id$id?itscg=10000&itsct=");
   final url = "https://apps.apple.com/in/story/id$id?itscg=10000&itsct=";
-  if (await url_luncher.canLaunchUrl(Uri.parse(url))) {
-    await url_luncher.launchUrl(Uri.parse(url),
-        mode: url_luncher.LaunchMode.externalApplication);
+  if (await url_luncher.canLaunch(url)) {
+    await url_luncher.launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+      headers: <String, String>{'my_header_key': 'my_header_value'},
+      // mode: url_luncher.LaunchMode.externalApplication
+    );
   } else {
     snackBar(context);
   }
